788be0d4768efad5745f28f65fada44ed7c40183
Chris Roberts <chrisroberts.code@gmail.com>
2013-06-20 13:55:29 -0700

    Enable setuid by default. Use upstart if on ubuntu, otherwise default to runit

--- attributes/default.rb
+++ attributes/default.rb
@@ -5,4 +5,5 @@ default[:beaver][:outputs] = []
 default[:beaver][:format] = "json"
 default[:beaver][:user] = 'beaver'
 default[:beaver][:group] = 'beaver'
-default[:beaver][:init_type] = 'runit'
+default[:beaver][:init_type] = node[:platform] == 'ubuntu' ? 'upstart' : 'runit'
+default[:beaver][:supports_setuid] = true
